62d3d2cdc59b70e6269b24f57170d2b1c1ce8a54
Vatsal Hirpara <vatsal.hirpara@nagarro.com>
2021-05-13 12:00:14 +0530

    enable purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  // purge: {
-  //   enabled: true,
-  //   content: ['./public/**/*.html'],
-  // },
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
